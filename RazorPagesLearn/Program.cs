2452345f4ac75e3d24a93f141357bfb3072ae4c1
Дмитрий Kernel <kernel@deeplay.io>
2022-06-07 22:32:43 +0700

    Add base metrics and support docker

--- RazorPagesLearn/Program.cs
+++ RazorPagesLearn/Program.cs
@@ -1,11 +1,37 @@
+using App.Metrics.Formatters.Prometheus;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using RazorPagesLearn.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddSingleton<IEmployeeRepository, MokEmployRepository>();
-
+builder.WebHost.UseMetricsEndpoints(options =>
+{
+    options.EnvironmentInfoEndpointEnabled = false;
+    options.MetricsTextEndpointOutputFormatter = new MetricsPrometheusTextOutputFormatter();
+    options.MetricsEndpointOutputFormatter = new MetricsPrometheusProtobufOutputFormatter();
+});
+builder.WebHost.UseMetricsWebTracking(options =>
+{
+    options.OAuth2TrackingEnabled = true;
+});
+/*builder.WebHost.ConfigureKestrel(serverOptions => 
+{
+    serverOptions.ListenAnyIP(7287 ,listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http1;
+    });
+    serverOptions.ListenAnyIP(5001, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http2;
+    });
+});*/
+/*ConfigureWebHostDefaults(webBuilder => webBuilder.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8080, listenOptions => listenOptions.Protocols = HttpProtocols.Http1);
+    options.ListenAnyIP(5000, listenOptions => listenOptions.Protocols = HttpProtocols.Http2);
+});*/
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
