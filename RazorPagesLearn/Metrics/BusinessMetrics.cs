92f90dd367415599820c43baef3e46bbbb31d938
Дмитрий Kernel <kernel@deeplay.io>
2022-06-09 15:49:42 +0700

    Add metrics in project

---
+++ RazorPagesLearn/Metrics/BusinessMetrics.cs
@@ -0,0 +1,16 @@
+﻿using App.Metrics;
+using App.Metrics.Counter;
+
+namespace RazorPagesLearn.Metrics
+{
+    public static class BusinessMetrics
+    {
+        public static CounterOptions CounterAddButton => new()
+        {
+            Context = "AddButton",
+            Name = "CounterAddButton",
+            MeasurementUnit = Unit.Calls,
+            Tags = new MetricTags(new[] { "microservice1" }, new [] {"NumberOne"})
+        };
+    }
+}
--- RazorPagesLearn/Pages/Employees/Details.cshtml.cs
+++ RazorPagesLearn/Pages/Employees/Details.cshtml.cs
@@ -1,5 +1,7 @@
+using App.Metrics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesLearn.Metrics;
 using RazorPagesLearn.Models;
 using RazorPagesLearn.Services;
 
@@ -8,14 +10,17 @@ namespace RazorPagesLearn.Pages.Employees
     public class DetailsModel : PageModel
     {
         private readonly IEmployeeRepository employeeRepository;
-        public DetailsModel(IEmployeeRepository employeeRepository)
+        private readonly IMetrics metrics;
+        public DetailsModel(IEmployeeRepository employeeRepository, IMetrics metrics)
         {
+            this.metrics = metrics;
             this.employeeRepository = employeeRepository;
         }
         public employee Employee { get; set; } 
 
         public IActionResult OnGet(int id)
         {
+            metrics.Measure.Counter.Increment(BusinessMetrics.CounterAddButton);
             Employee = employeeRepository.GetEmployee(id);
             if (Employee == null)
             {
